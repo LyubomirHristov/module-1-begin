080d1003cb80db5f5a8a68cdfca99613bff7154a
Berik <lhristov47@gmail.com>
2016-12-22 07:47:23 +0200

    module 2, video 1- adding comments

---
+++ Module2/Module2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			//дефиниране на променливи
+
+			//събиране на числа
+
+			//писане в конзола
+
+
+		}
+	}
+}
