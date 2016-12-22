da7623adf1782271925df370ad01471f4aa069c0
Berik <lhristov47@gmail.com>
2016-12-22 08:08:24 +0200

    module2, video 2- variables type int

--- Module2/Module2/Program.cs
+++ Module2/Module2/Program.cs
@@ -8,11 +8,17 @@ namespace Module2
 		{
 
 			//дефиниране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
 
 			//събиране на числа
-
+			a = 55; b = 10; sum = a + b;
 			//писане в конзола
-
+			Console.Write (a);
+			Console.Write (" + ");
+			Console.Write (b);
+			Console.Write(" = ");
+			Console.WriteLine (sum);
 
 		}
 	}
