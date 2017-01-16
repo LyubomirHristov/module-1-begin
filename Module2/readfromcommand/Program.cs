252fa1628780894ca5dd329dc73c72adfc9d148a
Berik <lhristov47@gmail.com>
2017-01-16 14:40:44 +0200

    module 2, video 5  reading from row

---
+++ Module2/readfromcommand/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace readfromcommand
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//define the strings
+			int a = 5, b = 0;
+			//input data
+			Console.WriteLine("please input value for b: ");
+			b= Convert.ToInt32 ( Console.ReadLine() );
+			// the result
+			Console.WriteLine(" the sum of a and b is: " + ( a + b).ToString() + "\n\n");//слагаме .tostring-> за да ни превърне резултата в текст
+
+		}
+	}
+}
